8fffcc0a7223914d70db38262d16127f166abfcf
da9dac <da9dac@gmail.com>
2023-09-20 21:28:28 +0900

    init: 초기설정

---
+++ src/main/java/com/none/no_name/NoNameApplication.java
@@ -0,0 +1,13 @@
+package com.none.no_name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/none/no_name/NoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.none.no_name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
